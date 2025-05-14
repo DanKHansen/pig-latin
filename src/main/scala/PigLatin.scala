fec8a63541bc562a727b5c72b21d6248cd284a91
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-05-14 12:35:30 +0200

    solution

--- src/main/scala/PigLatin.scala
+++ src/main/scala/PigLatin.scala
@@ -1,10 +1,13 @@
 object PigLatin:
-   private val vowels = Seq('a', 'e', 'i', 'o', 'u')
-   private val consonants = ('a' to 'z').diff(vowels)
+   private val cs = ('a' to 'z').diff(Seq('a', 'e', 'i', 'o', 'u'))
    def translate(s: String): String =
-      s match
-         case s1 if vowels.contains(s1.head) || Seq("xr", "yt").contains(s1.take(2)) => s + "ay"
-         case s22 if s22.take(2).forall(consonants.contains(_))                      => s22.drop(2) + s22.take(2) + "ay"
-         case s2 if consonants.contains(s2.head)                                     => s2.drop(1) + s2.head + "ay"
-         case s3 if s3.contains("qu")                                                => "ay"
-         case s4 if s4.contains("y")                                                 => "ay"
+      s.split(" ")
+         .map {
+            case w if !w.headOption.exists(cs.contains) || w.startsWith("xr") || w.startsWith("yt") => w + "ay"
+            case s @ w                                                                              =>
+               val cc = w.iterator.takeWhile(c => cs.contains(c) && (c != 'y' || w.startsWith("y"))).toList.mkString
+               s.replaceFirst(cc, "") match
+                  case s"u$s" if cs.endsWith("q") => s"$s${cs}uay"
+                  case s                          => s"$s${cs}ay"
+
+         }.mkString(" ")
