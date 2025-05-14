eadebc984d1733df3e95095052c24ef1ed5a2f8d
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-05-14 12:56:49 +0200

    refactor

--- src/main/scala/PigLatin.scala
+++ src/main/scala/PigLatin.scala
@@ -1,12 +1,11 @@
 object PigLatin:
    private val cs = ('a' to 'z').diff(Seq('a', 'e', 'i', 'o', 'u'))
    def translate(s: String): String =
-      s.split(" ")
-         .map {
-            case w if !w.headOption.exists(cs.contains) || w.startsWith("xr") || w.startsWith("yt") => w + "ay"
-            case s @ w                                                                              =>
-               val cc = w.iterator.takeWhile(c => cs.contains(c) && (c != 'y' || w.startsWith("y"))).toList.mkString
-               s.replaceFirst(cc, "") match
-                  case s"u$s" if cc.endsWith("q") => s"$s${cc}uay"
-                  case s                          => s"$s${cc}ay"
-         }.mkString(" ")
+      s.split(" ").map {
+        case w if !w.headOption.exists(cs.contains) || w.startsWith("xr") || w.startsWith("yt") => w + "ay"
+        case s @ w                                                                              =>
+           val cc = w.iterator.takeWhile(c => cs.contains(c) && (c != 'y' || w.startsWith("y"))).toList.mkString
+           s.replaceFirst(cc, "") match
+              case s"u$s" if cc.endsWith("q") => s"$s${cc}uay"
+              case s                          => s"$s${cc}ay"
+      }.mkString(" ")
